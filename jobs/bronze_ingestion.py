d237b749196698fd3502ee12d6734f7a27c6e339
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-24 21:28:24 -0500

    Create silver transformation job

---
+++ jobs/bronze_ingestion.py
@@ -0,0 +1,8 @@
+from utils.schema import shipment_schema
+from utils.helpers import ensure_dir
+from config import SOURCE_FILE, BRONZE_PATH
+
+
+def run_bronze_job(spark):
+    print("Running Bronze Ingestion...")
+    return df
