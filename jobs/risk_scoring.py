b5dfec4832ba6cad9639c3f4bf204644adfb82f8
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-25 22:28:12 -0500

    Add multi-level scoring

--- jobs/risk_scoring.py
+++ jobs/risk_scoring.py
@@ -4,5 +4,14 @@ from utils.helpers import ensure_dir
 
 
 def run_risk_scoring(spark):
+    print("Running Risk Scoring...")
+
+    ensure_dir(RISK_PATH)
+
+    df = spark.read.parquet(SLA_PATH)
+
+    scored = df.withColumn(
+        "risk_score"
+    )
 
     return scored
