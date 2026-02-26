b19232b768032b258531d6d87cfe7878aeaadf77
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-25 22:24:12 -0500

    Add SLA threshold config

--- jobs/sla_detection.py
+++ jobs/sla_detection.py
@@ -4,5 +4,15 @@ from utils.helpers import ensure_dir
 
 
 def run_sla_detection(spark):
+    print("Running SLA Detection...")
+
+    ensure_dir(SLA_PATH)
+
+    df = spark.read.parquet(SILVER_PATH)
+
+    flagged = df.withColumn(
+        "sla_breach",
+        when(col("delivery_days") > SLA_THRESHOLD_DAYS, "BREACH")
+    )
 
     return flagged
