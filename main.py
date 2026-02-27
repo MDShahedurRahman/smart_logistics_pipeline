7066fd310a611d42ae3143d01aaa06756f64a1c3
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-27 00:07:39 -0500

    Create main entry point

---
+++ main.py
@@ -0,0 +1,15 @@
+from utils.spark_session import get_spark_session
+
+from jobs.bronze_ingestion import run_bronze_job
+from jobs.silver_transformation import run_silver_job
+from jobs.sla_detection import run_sla_detection
+from jobs.risk_scoring import run_risk_scoring
+from jobs.gold_star_schema import run_gold_job
+from jobs.business_kpis import run_kpi_job
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
