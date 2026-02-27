5b54a35e2fa65fbbcc76fe8139c7f5a5f237462e
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-26 23:57:10 -0500

    Implement dim_customer

--- jobs/gold_star_schema.py
+++ jobs/gold_star_schema.py
@@ -3,3 +3,10 @@ from utils.helpers import ensure_dir
 
 
 def run_gold_job(spark):
+    print("Running Gold Layer...")
+
+    ensure_dir(GOLD_PATH)
+
+    df = spark.read.parquet(RISK_PATH)
+
+    dim_customer = df.select("customer_id").dropDuplicates()
